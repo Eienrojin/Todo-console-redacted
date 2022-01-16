4ddefd0a17dcf12855f3495cd1189c707f3fc91a
Nikita Polunin (二木立・半人) <77877550+Eienrojin@users.noreply.github.com>
2022-01-16 12:17:11 +0300

    Update Program.cs

--- Todo console redacted/Program.cs
+++ Todo console redacted/Program.cs
@@ -1,5 +1,7 @@
-﻿List<string> taskList = new List<string>();
+List<string> taskList = new List<string>();
 taskList.Add("Тестовая запись");
+taskList.Add("Тестовая запись 2");
+taskList.Add("Тестовая запись 3");
 
 //Чтение записанного файла написать тут
 //
@@ -20,9 +22,9 @@ void MainMenu()
         Console.WriteLine("Что вы хотите сделать?" +
         "\n1. Добавить задачу" +
         "\n2. Удалить задачу" +
-        "\n3. ---" +
+        "\n3. Изменить задачу" +
         "\n4. ---" +
-        "\nexit - выход");
+        "\n\"выход\" - для выхода");
 
         string answer = Console.ReadLine().Trim().ToLower();
 
@@ -32,19 +34,23 @@ void MainMenu()
                 WarningMessage.SendMessage();
                 break;
             case "1":
+                Console.Clear();
                 InitTask();
                 break;
             case "2":
                 Console.Clear();
 
-                if (CheckDeleteChoice.CheckChoice(taskList))
+                if (CheckDeleteChoice.CheckLength(taskList, 2) == true)
                     DeleteTask();
                 break;
             case "3":
+                Console.Clear();
+                EditTask();
                 break;
             case "4":
+                Console.Clear();
                 break;
-            case "exit":
+            case "выход":
                 Environment.Exit(0);
                 break;
         }
@@ -65,6 +71,8 @@ void InitTask()
             "\nC - Маловажное");
 
         string priority = InitChoiceOrBack().ToUpper();
+        if (priority == "А" || priority == "В" || priority == "С") //В условии русские символы
+            priority = RussToEng(priority);
 
         if (priority == "A" || priority == "B" || priority == "C")
         {
@@ -80,6 +88,58 @@ void InitTask()
     }
 }
 
+void DeleteTask()
+{
+    ShowTaskList();
+
+    Console.WriteLine("Введите номер записи, которую вы хотите удалить...");
+
+    string choice = InitChoiceOrBack();
+    int intChoice = 0;
+
+    try
+    {
+        intChoice = Convert.ToInt32(choice);
+    }
+    catch (FormatException)
+    {
+        WarningMessage.SendMessage();
+        DeleteTask();
+    }
+
+    taskList.RemoveAt(intChoice - 1);
+}
+
+void EditTask()
+{
+    ShowTaskList();
+
+    Console.WriteLine("Введите номер записи, которую вы хотите изменить...");
+
+    string choice = InitChoiceOrBack();
+    int indexOfList = 0;
+
+    try
+    {
+        indexOfList = Convert.ToInt32(choice);
+    }
+    catch (FormatException)
+    {
+        WarningMessage.SendMessage();
+        EditTask();
+    }
+
+    indexOfList -= 1;
+
+    Console.Write($"{taskList.ElementAt(indexOfList)} --> ");
+
+    choice = InitChoiceOrBack();
+
+    taskList.RemoveAt(indexOfList);
+    taskList.Insert(indexOfList, choice);
+}
+
+//Второстепенные методы 
 void ShowTaskList()
 {
     int counter = 1;
@@ -95,36 +155,29 @@ void ShowTaskList()
     Console.WriteLine("\n----------\n");
 }
 
-void DeleteTask()
-{
-    Console.Clear();
-    ShowTaskList();
-
-    Console.WriteLine("Введите номер записи, которую вы хотите удалить... ");
-
-    int intChoice = 0;
-    string choice = InitChoiceOrBack();
-
-    try
-    {
-        intChoice = Convert.ToInt32(choice);
-        taskList.RemoveAt(intChoice - 1);
-    }
-    catch (ArgumentOutOfRangeException)
-    {
-        WarningMessage.SendMessage();
-        DeleteTask();
-    }
-}
-
 string InitChoiceOrBack()
 {
-    Console.WriteLine("Если хотите вернуться назад, введите return");
+    Console.WriteLine("Если хотите вернуться назад, введите \"назад\"");
 
     string choice = Console.ReadLine().Trim();
 
-    if (choice.ToLower() == "return")
+    if (choice.ToLower() == "назад")
         MainMenu();
+
+    return choice;
+}
+
+//Конвертация русских символов в английские
+string RussToEng(string choice)
+{
+    if (choice == "А")
+        choice = "A";
+
+    if (choice == "В")
+        choice = "B";
+
+    if (choice == "С")
+        choice = "C";
 
     return choice;
 }
@@ -134,15 +187,15 @@ class WarningMessage
     protected bool Safe { get; set; }
     protected string Message { get; set; }
 
+    //Для вывода ошибки внутри чей-то логики с выбором надписи
     protected void SendMessage(string message)
     {
-        Message = message;
-
-        Console.WriteLine(Message +
+        Console.WriteLine(message +
             "\nДля продолжения нажмите любую клавишу...");
         Console.ReadKey();
     }
 
+    //Для простого вывода ошибки
     public void SendMessage()
     {
         Console.WriteLine("Что-то пошло не так, проверьте правильность выбора." +
@@ -151,10 +204,27 @@ class WarningMessage
     }
 }
 
-//Попробовать использовать виртуальный метод
 class CheckDeleteChoice : WarningMessage
 {
-    public bool CheckChoice(List<string> List)
+    public bool CheckLength(List<string> List, int value)
+    {
+        if (List.Count() < value)
+        {
+            Safe = false;
+            SendMessage("Записей слишком мало для удаления");
+
+            return Safe;
+        }
+        else
+        {
+            Safe = true;
+
+            return Safe;
+        }
+    }
+
+    //допилить
+    public bool CheckOutOfRange(List<string> List)
     {
         if (List.Count() < 2)
         {
